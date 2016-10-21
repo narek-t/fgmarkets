c2a81b5e3242da76dc65a8f147665d9dfd98c66a
Narek <narek@Nareks-iMac.local>
2016-10-21 16:36:24 +0300

    -1

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,5 +5,36 @@ $(document).ready(function() {
 		nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         spaceBetween: 80,
+        breakpoints: {
+        	992: {
+        		spaceBetween: 40,
+        	},
+        	768: {
+        		slidesPerView: 1,
+        	},
+        }
+	});
+	$(".toggle-mobile").click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+		$(".nav-row ul").toggleClass('active');
+	});
+	var clientssSlider = new Swiper ('.clients-slider', {
+		loop: true,
+		slidesPerView: 4,
+		nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 80,
+        breakpoints: {
+        	992: {
+        		spaceBetween: 40,
+        	},
+        	768: {
+        		slidesPerView: 2,
+        	},
+        	480: {
+        		slidesPerView: 1,
+        	},
+        }
 	});
 });

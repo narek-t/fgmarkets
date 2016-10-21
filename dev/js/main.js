869f945ac35455cc25fc22777cf3242f9d9c4495
Narek <narek@Nareks-iMac.local>
2016-10-21 10:24:18 +0300

    1

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
+	var stepsSlider = new Swiper ('.steps-slider', {
+		loop: true,
+		slidesPerView: 3,
+		nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 80,
+	});
 });
